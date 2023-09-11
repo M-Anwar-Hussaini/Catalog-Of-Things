a2bedd11412b741f6d2e2718ac7110af36882d85
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-11 22:53:17 +0430

    Implement other methods for item class.

--- src/item.rb
+++ src/item.rb
@@ -9,4 +9,26 @@ class Item
     @publish_date = Date.parse(publish_date)
     @archieved = can_be_archieved?
   end
+
+  def move_to_archive
+    @archieved = can_be_archieved?
+  end
+
+  def publish_date=(date_str)
+    @publish_date = Date.parse(date_str)
+    move_to_archive
+  end
+
+  def can_be_archieved?
+    Date.today.year - publish_date.year > 10
+  end
+
+  def to_s
+    details = ''
+    details << "\tItem: #{self.class}\n"
+    details << "\tID: #{id}\n"
+    details << "\tPublish date: #{publish_date}\n"
+    details << "\tArchieved: #{archieved}\n"
+    details
+  end
 end
