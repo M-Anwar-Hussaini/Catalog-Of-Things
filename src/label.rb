10b1aee12086e546f722ad7172a45bcae6341b64
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 04:15:56 +0430

    Initialize the Label class

--- src/label.rb
+++ src/label.rb
@@ -3,4 +3,10 @@ require_relative 'item'
 class Label
   attr_accessor :name
   attr_reader :id, :items
+
+  def initialize(name, id: nil)
+    @id = id.nil? ? rand(1...1000) : id
+    @name = name
+    @items = []
+  end
 end
