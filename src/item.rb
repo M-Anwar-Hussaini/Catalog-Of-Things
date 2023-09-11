89707cb79f489576eb1fa51be1234832c16c7b6a
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-11 22:52:03 +0430

    Implement the constructor method for item class.

--- src/item.rb
+++ src/item.rb
@@ -3,4 +3,10 @@ require 'date'
 class Item
   attr_accessor :genre, :author, :source, :label
   attr_reader :id, :archieved, :publish_date
+
+  def initialize(publish_date, id: nil)
+    @id = id.nil? ? rand(1...1000) : id
+    @publish_date = Date.parse(publish_date)
+    @archieved = can_be_archieved?
+  end
 end
