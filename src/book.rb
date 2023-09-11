0e4d0717dd5f92e4a7b2cd867bf4268739d8caad
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 03:52:02 +0430

    Create constructor for the Book class and initialize the values.

--- src/book.rb
+++ src/book.rb
@@ -3,4 +3,9 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
+  def initialize(publisher, cover_state, publish_date, id: nil)
+    @publisher = publisher
+    @cover_state = cover_state
+    super(publish_date, id: id)
+  end
 end
