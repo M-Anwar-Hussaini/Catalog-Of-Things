a4cab04fc2fad1c6ff2d9e2896974e716e95f6da
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 11:28:52 +0430

    Add test for label class and its constructor.

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,1 +1,19 @@
-# Tests for label
+require 'rspec'
+require_relative '../src/label'
+
+describe Label do
+  describe '#initialize' do
+    label = Label.new('Marked', id: 120)
+    it 'The name of label is Marked' do
+      expect(label.name).to eq('Marked')
+    end
+
+    it 'The id of label is from Integer type' do
+      expect(label.id).to be_an_instance_of(Integer)
+    end
+
+    it 'The id of label should be 120' do
+      expect(label.id).to eq(120)
+    end
+  end
+end
