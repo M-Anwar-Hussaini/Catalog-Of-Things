ecc6f1df584caa9f4fdaf4b29c0a70709b04d1c6
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-12 04:31:10 +0300

    Set up author class properties in the constructor

--- src/author.rb
+++ src/author.rb
@@ -1,3 +1,11 @@
-class Author
 
+require_relative 'item'
+
+class Author
+  def initialize(first_name, last_name, id: nil)
+    @id = id.nil? ? rand(1...1000) : id
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
+    @items = []
+  end
 end
