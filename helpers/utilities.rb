d5a1ca7862cc1a686b5a0c507188982567bfec8f
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 20:48:48 +0430

    Add display_option method and fix the linters

--- helpers/utilities.rb
+++ helpers/utilities.rb
@@ -1,5 +1,26 @@
 module Utilities
-  
+  def get_array_index(array, title, prompt)
+    display_array(array, title)
+    print prompt
+    gets.chomp.to_i - 1
+  end
+
+  def display_options
+    puts 'Available Operations: '
+    puts '1 - To create an Author.'
+    puts '2 - To create a Genre.'
+    puts '3 - To create a Label.'
+    puts '4 - To create a Source.'
+    puts '5 - To create an Item.'
+    puts '6 - To list all Authors.'
+    puts '7 - To list all Genres.'
+    puts '8 - To list all Labels.'
+    puts '9 - To list all Sources.'
+    puts '10 - To List all Items.'
+    puts '0 - to quit the application.'
+    print 'Please select a number to do its operation. [Integer]: '
+  end
+
   def display_array(arr, title)
     puts
     puts title
