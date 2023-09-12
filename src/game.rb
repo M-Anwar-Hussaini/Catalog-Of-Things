3159ad712c0092a87e416f18bea840a6548ff213
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-12 04:09:54 +0300

    Add accessors for game class properties

--- src/game.rb
+++ src/game.rb
@@ -2,6 +2,8 @@ require 'date'
 require_relative 'item'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(multiplayer, last_played_at, publish_date, id: nil)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
