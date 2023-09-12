1cddb1787a5372cdf5f5b28642814f088050c184
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-12 04:04:33 +0300

    Set up game class properties in the constructor

--- src/game.rb
+++ src/game.rb
@@ -1,5 +1,11 @@
+require 'date'
 require_relative 'item'
 
 class Game < Item
+  def initialize(multiplayer, last_played_at, publish_date, id: nil)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+    super(publish_date, id: id)
+  end
 
 end
