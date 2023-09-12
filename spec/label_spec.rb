c03a968c6e22609eb649de6bf467388f0ed8fb25
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 11:44:22 +0430

    Add before block for each test group.

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -3,40 +3,45 @@ require_relative '../src/label'
 
 describe Label do
   describe '#initialize' do
-    label = Label.new('Marked', id: 120)
+    before do
+      @label = Label.new('Marked', id: 120)
+    end
+    
     it 'The name of label is Marked' do
-      expect(label.name).to eq('Marked')
+      expect(@label.name).to eq('Marked')
     end
 
     it 'The id of label is from Integer type' do
-      expect(label.id).to be_an_instance_of(Integer)
+      expect(@label.id).to be_an_instance_of(Integer)
     end
 
     it 'The id of label should be 120' do
-      expect(label.id).to eq(120)
+      expect(@label.id).to eq(120)
     end
 
     it 'Currently Label has not item' do
-      expect(label.items).to be_empty
+      expect(@label.items).to be_empty
     end
   end
 
   describe '#respond_to' do
-    label = Label.new('Expired')
+    before do
+      @label = Label.new('Expired')
+    end
     it 'Label class has the id attribute' do
-      expect(label).to respond_to(:id)
+      expect(@label).to respond_to(:id)
     end
 
     it 'Label class has the name attribute' do
-      expect(label).to respond_to(:name)
+      expect(@label).to respond_to(:name)
     end
 
     it 'Label class has the items attribute' do
-      expect(label).to respond_to(:items)
+      expect(@label).to respond_to(:items)
     end
 
     it 'Label class has the add_item method' do
-      expect(label).to respond_to(:add_item)
+      expect(@label).to respond_to(:add_item)
     end
   end
 end
