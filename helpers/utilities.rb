3f22f18d0558e391f305def46695d07c85ee7a18
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 20:46:03 +0430

    Create display_array method

--- helpers/utilities.rb
+++ helpers/utilities.rb
@@ -1,4 +1,15 @@
 module Utilities
+  
+  def display_array(arr, title)
+    puts
+    puts title
+    arr.each_with_index do |item, index|
+      puts "#{index + 1}) #{item}"
+    end
+    puts 'The list is empty.' if arr.empty?
+    puts
+  end
+
   def get_text(prompt)
     print prompt
     gets.chomp
