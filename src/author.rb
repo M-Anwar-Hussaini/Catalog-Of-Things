2f1d89a0251467612325396d00a2808c74f724fa
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-12 04:36:35 +0300

    Add accessors for author class properties

--- src/author.rb
+++ src/author.rb
@@ -1,7 +1,9 @@
-
 require_relative 'item'
 
 class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
   def initialize(first_name, last_name, id: nil)
     @id = id.nil? ? rand(1...1000) : id
     @first_name = first_name.capitalize
