aa17601eda0c310da8fab4f3df5261032b3c29dc
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-12 11:17:12 +0430

    Add test for can_be_archieved method.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -18,5 +18,15 @@ describe Book, Item do
     end
   end
 
+  describe '#can_be_archieved?' do
+    it 'This book is can not be archieved for now' do
+      expect(book.can_be_archieved?).to eq(false)
+    end
+
+    it 'A book with bad cover state can be archieved.' do
+      expect(Book.new('Netflix', 'Bad', '1999-2-2').can_be_archieved?).to eq(true)
+    end
+  end
+
   
 end
