4bf66a4d8cbc3bed731b58fa088e347290dc4ed6
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-12 09:37:03 -0500

    Create MusicAlbum class

--- src/music_album.rb
+++ src/music_album.rb
@@ -1,1 +1,10 @@
-# Code for music album
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify, publish_date, id: nil)
+    super(publish_date, id: id)
+    @on_spotify = on_spotify
+  end
+end
