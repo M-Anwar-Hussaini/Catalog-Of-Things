93c0dfe8394d0646b713b926f4ce7db1b239f926
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-12 09:45:30 -0500

    Create Genre class

--- src/genre.rb
+++ src/genre.rb
@@ -1,1 +1,12 @@
-# Code for genre
+require_relative 'item'
+
+class Genre
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialize(name, id: nil)
+    @id = id.nil? ? rand(1...1000) : id
+    @name = name
+    @items = []
+  end
+end
