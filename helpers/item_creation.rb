313c7ce963154f4107b5d095b9426bdfda47bac9
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-13 07:39:21 +0300

    Create method create_book

--- helpers/item_creation.rb
+++ helpers/item_creation.rb
@@ -1,3 +1,8 @@
 module ItemCreation
-
+  def create_book
+    publisher = get_text('Please enter the book\'s publisher: ')
+    cover_state = get_text("Please enter book's cover state: ")
+    publish_date = get_text("Please enter book's publish date in the format of [yyyy-mm-dd]: ")
+    Book.new(publisher, cover_state, publish_date)
+  end
 end
