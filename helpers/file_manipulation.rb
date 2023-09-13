c08dcb456d5865501bc8baeea681ee525fe6e8e4
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-13 09:19:01 -0500

    Add create_default_file method

--- helpers/file_manipulation.rb
+++ helpers/file_manipulation.rb
@@ -2,4 +2,9 @@ module FileManipulation
   def create_folder(folder_name)
     Dir.mkdir(folder_name) unless File.directory?(folder_name)
   end
+
+  def create_default_file(path)
+    default = JSON.pretty_generate([])
+    File.write(path, default) unless File.exist?(path)
+  end
 end
