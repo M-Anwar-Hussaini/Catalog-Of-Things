833add1d59b84e9a086c5616c0a753a6117c8d43
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-13 10:09:51 +0430

    Build create album_ob method to convert a hash to an album object

--- helpers/file_to_array.rb
+++ helpers/file_to_array.rb
@@ -4,4 +4,11 @@ module FileToArray
   def read_file(path)
     JSON.parse(File.read(path))
   end
+
+  def create_album_obj(album_hash)
+    on_spotify = album_hash['On Spotify']
+    publish_date = album_hash['Publish Date']
+    id = album_hash['ID']
+    MusicAlbum.new(on_spotify, publish_date, id: id)
+  end
 end
