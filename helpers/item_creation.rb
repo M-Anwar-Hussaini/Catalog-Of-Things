7f7f728114b6313ffb9bbfb722fc1bdb0e715525
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-13 07:39:50 +0300

    Create method create_game

--- helpers/item_creation.rb
+++ helpers/item_creation.rb
@@ -5,4 +5,11 @@ module ItemCreation
     publish_date = get_text("Please enter book's publish date in the format of [yyyy-mm-dd]: ")
     Book.new(publisher, cover_state, publish_date)
   end
+
+  def create_game
+    multiplayer = get_text('Is the game multiplayer [Y/N]: ').downcase == 'y'
+    last_played_at = get_text('Please enter the last played date of the game in this format. [yyyy-mm-dd]: ')
+    publish_date = get_text("Please enter game's publish date in the format of [yyyy-mm-dd]: ")
+    Game.new(multiplayer, last_played_at, publish_date)
+  end
 end
