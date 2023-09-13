735617ce6e405b360d6497676c8a0ed435bc2dec
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-13 07:11:28 +0300

    Create method authors

--- helpers/array_to_json.rb
+++ helpers/array_to_json.rb
@@ -1,5 +1,10 @@
 require 'json'
 
 module ArrayToJson
-
+  def authors_to_json(authors)
+    result = authors.map do |author|
+      create_author_hash(author)
+    end
+    JSON.pretty_generate(result)
+  end
 end
