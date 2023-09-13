c033aca0e97a63ea9d671a4bf8d305eed81b1334
lRebornsl <anthony_vasquez98@hotmail.com>
2023-09-13 09:22:16 -0500

    Add create_files_folders method

--- helpers/file_manipulation.rb
+++ helpers/file_manipulation.rb
@@ -1,4 +1,8 @@
+require_relative 'paths'
+
 module FileManipulation
+  include Paths
+
   def create_folder(folder_name)
     Dir.mkdir(folder_name) unless File.directory?(folder_name)
   end
@@ -6,5 +10,14 @@ module FileManipulation
   def create_default_file(path)
     default = JSON.pretty_generate([])
     File.write(path, default) unless File.exist?(path)
+  end
+
+  def create_files_folders
+    create_folder('json')
+    create_default_file(AUTHORS_PATH)
+    create_default_file(GENRES_PATH)
+    create_default_file(LABELS_PATH)
+    create_default_file(SOURCES_PATH)
+    create_default_file(ITEMS_PATH)
   end
 end
