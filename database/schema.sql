aa0042cb6cf7be1cc7bb338b415486552e71fb44
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-14 04:24:49 +0300

    Create games table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE authors (
     first_name VARCHAR(50),
     last_name VARCHAR(50)
 );
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    multiplayer BOOL,
+    last_played_at DATE,
+    publish_date DATE,
+    archieved BOOL,
+    genre_id INTEGER REFERENCES genres(id),
+    author_id INTEGER REFERENCES authors(id),
+    source_id INTEGER REFERENCES sources(id),
+    label_id INTEGER REFERENCES labels(id)
+);
