121f45efe10cb7d0fbe1096a5e9b8d739e35bd16
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-09-14 20:53:57 +0430

    Finialize database schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,21 +1,14 @@
-CREATE DATABASE catalog
+CREATE DATABASE catalog.
 
-CREATE TABLE authors (
-    id	SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50)
+CREATE TABLE labels (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(50),
+	color VARCHAR(50)
 );
 
-CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
-    multiplayer BOOL,
-    last_played_at DATE,
-    publish_date DATE,
-    archieved BOOL,
-    genre_id INTEGER REFERENCES genres(id),
-    author_id INTEGER REFERENCES authors(id),
-    source_id INTEGER REFERENCES sources(id),
-    label_id INTEGER REFERENCES labels(id)
+CREATE TABLE sources (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
 );
 
 CREATE TABLE genres (
@@ -23,9 +16,50 @@ CREATE TABLE genres (
 	name VARCHAR(50)
 );
 
+CREATE TABLE authors (
+	id	SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+
+CREATE TABLE books (
+	id SERIAL PRIMARY KEY,
+	publisher VARCHAR(100),
+	cover_state VARCHAR(50),
+	publish_date DATE,
+	archieved BOOL,
+	genre_id INTEGER REFERENCES genres(id),
+	author_id INTEGER REFERENCES authors(id),
+	source_id INTEGER REFERENCES sources(id),
+	label_id INTEGER REFERENCES labels(id)
+);
+
 CREATE TABLE music_albums (
 	id SERIAL PRIMARY KEY,
 	on_spotify BOOL,
+	publish_date DATE,
+	archieved BOOL,
+	genre_id INTEGER REFERENCES genres(id),
+	author_id INTEGER REFERENCES authors(id),
+	source_id INTEGER REFERENCES sources(id),
+	label_id INTEGER REFERENCES labels(id)
+);
+
+CREATE TABLE movies (
+	id SERIAL PRIMARY KEY,
+	silent BOOL,
+	publish_date DATE,
+	archieved BOOL,
+	genre_id INTEGER REFERENCES genres(id),
+	author_id INTEGER REFERENCES authors(id),
+	source_id INTEGER REFERENCES sources(id),
+	label_id INTEGER REFERENCES labels(id)
+);
+
+CREATE TABLE games (
+	id SERIAL PRIMARY KEY,
+	multiplayer BOOL,
+  last_played_at DATE,
 	publish_date DATE,
 	archieved BOOL,
 	genre_id INTEGER REFERENCES genres(id),
