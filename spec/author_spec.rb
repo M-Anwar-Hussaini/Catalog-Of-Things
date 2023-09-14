8be2bffee4268571d344ec0e23775793f5669c76
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-09-14 06:19:47 +0300

    Create test for author class initialize

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,1 +1,20 @@
-# Tests for author
+require 'rspec'
+require_relative '../src/author'
+require_relative '../src/item'
+
+describe Author, Item do
+  author = Author.new('John', 'Doe')
+  describe 'Test class initialize' do
+    it 'The author is from the class of Author' do
+      expect(author).to be_an_instance_of(Author)
+    end
+
+    it 'The author has first name John' do
+      expect(author.first_name).to eq('John')
+    end
+
+    it 'The author has last name Doe' do
+      expect(author.last_name).to eq('Doe')
+    end
+  end
+end
